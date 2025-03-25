350f35bfc2682961f35183f52f5fe764c188dfb6
Ragna1204 <samarth.dun@gmail.com>
2025-03-26 02:47:37 +0530

    animations: idle, running, jump, attack

--- Scenes/entities/player/camera_controller.gd
+++ Scenes/entities/player/camera_controller.gd
@@ -4,7 +4,7 @@ extends Node3D
 @export var max_limit_x: float
 @export var horizontal_acceleration := 2.0
 @export var vertical_acceleration := 1.0
-@export var mouse_acceleration := 0.005
+@export var mouse_acceleration := 0.004
 
 func _process(delta: float) -> void:
 	var joy_dir = Input.get_vector("pan_left", "pan_right", "pan_up", "pan_down")
--- Scenes/entities/player/player.gd
+++ Scenes/entities/player/player.gd
@@ -14,11 +14,14 @@ extends CharacterBody3D
 
 @onready var camera = $CameraController/Camera3D
 
+@onready var skin = $GodetteSkin
+
 var movement_input := Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	move_logic(delta)
 	jump_logic(delta)
+	ability_logic()
 	move_and_slide()
 	
 	
@@ -34,15 +37,26 @@ func move_logic(delta) -> void:
 		vel_2d = vel_2d.limit_length(speed)
 		velocity.x = vel_2d.x
 		velocity.z = vel_2d.y
+		skin.set_move_state('Running_B')
+		var target_angle = -movement_input.angle() + PI/2
+		skin.rotation.y = rotate_toward(skin.rotation.y, target_angle, 10.0 * delta)
 	else:
 		vel_2d = vel_2d.move_toward(Vector2.ZERO, base_speed * 4.0 * delta)
 		velocity.x = vel_2d.x
 		velocity.z = vel_2d.y
+		skin.set_move_state('Idle')
 	
 	
 func jump_logic(delta) -> void:
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = -jump_velocity
+	else:
+		skin.set_move_state('Jump_Idle')
 	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
 	velocity.y -= gravity * delta
+	
+
+func ability_logic() -> void:
+	if Input.is_action_just_pressed("ability"):
+		skin.attack()
