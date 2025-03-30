a5cd1ae891f30d165bc469304babf81a70e65872
Ragna1204 <samarth.dun@gmail.com>
2025-03-30 23:35:45 +0530

    enemies animations and movement

--- Scenes/entities/enemies/enemy.gd
+++ Scenes/entities/enemies/enemy.gd
@@ -2,12 +2,17 @@ class_name Enemy
 extends CharacterBody3D
 
 @onready var move_state_machine = $AnimationTree.get("parameters/MoveStateMachine/playback")
+@onready var attack_animation = $AnimationTree.get_tree_root().get_node('AttackAnimation')
 @onready var player = get_tree().get_first_node_in_group('Player')
 @onready var skin = get_node('skin')
 
 @export var walk_speed := 2.0
+@export var speed = walk_speed
+var speed_modifier := 1.0
 @export var notice_radius := 30.0
 @export var attack_radius := 3.0
+
+var rng = RandomNumberGenerator.new()
 
 func move_to_player(delta):
 	if position.distance_to(player.position) < notice_radius:
@@ -16,9 +21,14 @@ func move_to_player(delta):
 		var target_angle = -target_vec2.angle() + PI/2
 		rotation.y = rotate_toward(rotation.y, target_angle, delta * 6.0)
 		if position.distance_to(player.position) > attack_radius:
-			velocity = Vector3(target_vec2.x, 0, target_vec2.y) * walk_speed
+			velocity = Vector3(target_vec2.x, 0, target_vec2.y) * speed * speed_modifier
 			move_state_machine.travel('walk')
 		else:
 			velocity = Vector3.ZERO
 			move_state_machine.travel('idle')
 		move_and_slide()
+
+func stop_movement(start_duration: float, end_duration: float):
+	var tween = create_tween()
+	tween.tween_property(self, "speed_modifier", 0.0, start_duration)
+	tween.tween_property(self, "speed_modifier", 1.0, end_duration)
